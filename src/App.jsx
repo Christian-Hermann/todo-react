4f2fc82a0c1891756af0ac34430ceaf80b61a992
Your NameChristian <christianhermanndev@gmail.com>
2026-01-08 13:11:12 -0600

    Added a todo button

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import { useState } from "react";
 export default function App() {
   const [todos, setTodos] = useState(["gym", "eat", "work"]);
 
+  function addTodo() {
+    setTodos([...todos, "New todo"]);
+  }
+
   return (
     <div>
       <h1>Todo App</h1>
+      <button onClick={addTodo}>Add todo</button>
       <ul>
         {todos.map((todo) => (
           <li key={todo}>{todo}</li>

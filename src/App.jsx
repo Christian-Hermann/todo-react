7dae3464d412fb439c67a8d4ca3e5c955e01d1c7
Your NameChristian <christianhermanndev@gmail.com>
2026-01-08 15:38:45 -0600

    Added input for text to add todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from "react";
 
 export default function App() {
   const [todos, setTodos] = useState(["gym", "eat", "work"]);
+  const [text, setText] = useState("");
 
   function addTodo() {
-    setTodos([...todos, "New todo"]);
+    setTodos([...todos, text]);
+    setText("");
   }
 
   return (
     <div>
       <h1>Todo App</h1>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
       <button onClick={addTodo}>Add todo</button>
       <ul>
         {todos.map((todo) => (

f618a2099755256ccefd826124616097a005f733
Your NameChristian <christianhermanndev@gmail.com>
2026-01-27 14:51:58 -0600

    Added pseudocode to TodoList

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,13 @@ import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 import "./App.css";
 
-// key name to save/load todos in local storage
+// Key name to save/load todos in local storage
 const STORAGE_KEY = "todos";
 
 // STATE for todos
-// runs once the app loads
-// if there are todos in local storage use them
-// otherwise use default todos
+// Runs once the app loads
+// If there are todos in local storage use them
+// Otherwise use default todos
 export default function App() {
   const [todos, setTodos] = useState(() => {
     const savedTodos = localStorage.getItem(STORAGE_KEY);
@@ -29,10 +29,10 @@ export default function App() {
   const [text, setText] = useState("");
 
   // FUNCTION to add todo
-  // creates new todo object using current text
-  // adds text to existing todos array
-  // updates state with new arrray
-  // then clears inputs
+  // Creates new todo object using current text
+  // Adds text to existing todos array
+  // Updates state with new arrray
+  // Then clears inputs
   function addTodo() {
     setTodos([...todos, { text: text, completed: false }]);
     setText("");
@@ -40,8 +40,8 @@ export default function App() {
 
   // FUNCTION toggleTodo
   // When clicked index is found switch completed true/false
-  // leave the other todos untouched
-  // then update state with new array
+  // Leave the other todos untouched
+  // Then update state with new array
   function toggleTodo(indexToggle) {
     const newTodos = todos.map((todo, index) => {
       if (index === indexToggle) {
@@ -54,17 +54,17 @@ export default function App() {
   }
 
   // FUNCTION deleteTodo
-  // creates a new array without the todo at indexDelete
-  // update state with new array
+  // Creates a new array without the todo at indexDelete
+  // Update state with new array
   function deleteTodo(indexDelete) {
     const newTodos = todos.filter((todo, index) => index !== indexDelete);
     setTodos(newTodos);
   }
 
   // EFFECT save todos to local storage
-  // this runs everytime todo state changes
-  // converts todos array to a string
-  // saves it under STORAGE_KEY
+  // This runs everytime todo state changes
+  // Converts todos array to a string
+  // Saves it under STORAGE_KEY
   useEffect(() => {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
   }, [todos]);
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,14 +1,23 @@
 export default function TodoList({ todos, toggleTodo, deleteTodo }) {
+  // COMPONENT
+  // Recieves todos from app by way of props
+  // Displays todos
+  // Calls Apps functions when the user interacts
   return (
     <ul>
       {todos.map((todo, index) => (
         <li key={index}>
+          {/* Checkbox shows todo.completed */}
+          {/* When changed it tells App which todo to toggle */}
           <input
             type="checkbox"
             checked={todo.completed}
             onChange={() => toggleTodo(index)}
           />
+          {/* Displays the todo text */}
           {todo.text}
+
+          {/* Delete button tells App which todo to remove */}
           <button onClick={() => deleteTodo(index)}>Delete</button>
         </li>
       ))}

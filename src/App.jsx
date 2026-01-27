9ffe57969ad71d25e14caafcd3b975a5b62994f4
Your NameChristian <christianhermanndev@gmail.com>
2026-01-27 12:59:48 -0600

    Add pseudocode to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,13 @@ import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 import "./App.css";
 
+// key name to save/load todos in local storage
 const STORAGE_KEY = "todos";
 
+// STATE for todos
+// runs once the app loads
+// if there are todos in local storage use them
+// otherwise use default todos
 export default function App() {
   const [todos, setTodos] = useState(() => {
     const savedTodos = localStorage.getItem(STORAGE_KEY);
@@ -20,13 +25,23 @@ export default function App() {
     ];
   });
 
+  // STATE for text
   const [text, setText] = useState("");
 
+  // FUNCTION to add todo
+  // creates new todo object using current text
+  // adds text to existing todos array
+  // updates state with new arrray
+  // then clears inputs
   function addTodo() {
     setTodos([...todos, { text: text, completed: false }]);
     setText("");
   }
 
+  // FUNCTION toggleTodo
+  // When clicked index is found switch completed true/false
+  // leave the other todos untouched
+  // then update state with new array
   function toggleTodo(indexToggle) {
     const newTodos = todos.map((todo, index) => {
       if (index === indexToggle) {
@@ -38,15 +53,25 @@ export default function App() {
     setTodos(newTodos);
   }
 
+  // FUNCTION deleteTodo
+  // creates a new array without the todo at indexDelete
+  // update state with new array
   function deleteTodo(indexDelete) {
     const newTodos = todos.filter((todo, index) => index !== indexDelete);
     setTodos(newTodos);
   }
 
+  // EFFECT save todos to local storage
+  // this runs everytime todo state changes
+  // converts todos array to a string
+  // saves it under STORAGE_KEY
   useEffect(() => {
     localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
   }, [todos]);
 
+  // RENDER
+  // TodoForm handles input and the add button
+  // TodoList displays todos and handles toggle/delete
   return (
     <div className="app">
       <h1>Todos...</h1>

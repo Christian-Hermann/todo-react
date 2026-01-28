6ee14dbeb6a87f80ae2879d73a6a3d920830d1f8
Your NameChristian <christianhermanndev@gmail.com>
2026-01-28 13:09:43 -0600

    Added pseudocode to TodoForm

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,7 +1,14 @@
 export default function TodoForm({ text, setText, addTodo }) {
+  // COMPONENT
+  // Controls the input
+  // Calls addTodo when the button is clicked
   return (
     <div>
+      {/* Value comes from App state */}
+      {/* onChange updates App state */}
       <input value={text} onChange={(e) => setText(e.target.value)} />
+
+      {/* Click the button to add a todo */}
       <button onClick={addTodo}>Add todo</button>
     </div>
   );
